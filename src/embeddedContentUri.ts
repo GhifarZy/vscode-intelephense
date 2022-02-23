9bf7532760e1d93574f792a63f34eebdf269ed1a
GhifarZy <63408193+GhifarZy@users.noreply.github.com>
2022-02-23 21:11:49 +0700

    nice

--- src/embeddedContentUri.ts
+++ src/embeddedContentUri.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 'use strict';
-
+//
 import { Uri } from 'vscode';
 
 export const EMBEDDED_CONTENT_SCHEME = 'embedded-content';
